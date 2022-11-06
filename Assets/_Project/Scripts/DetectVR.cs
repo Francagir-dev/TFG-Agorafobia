fffe7da96b6d97e31e1fdf5b056f3bfb16094b1f
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2022-11-06 13:31:26 +0100

    Comment DetectVR

--- Assets/_Project/Scripts/DetectVR.cs
+++ Assets/_Project/Scripts/DetectVR.cs
@@ -5,9 +5,9 @@ using UnityEngine.XR.Management;
 
 public class DetectVR : MonoBehaviour
 {
-    public bool startVR;
-    public GameObject xrOrigin;
-    public GameObject desktopCharacter;
+    public bool startVR; //Bool for test in editor
+    public GameObject xrOrigin; //XR Character
+    public GameObject desktopCharacter; //Character when non-vr
 
 
     // Start is called before the first frame update
@@ -15,33 +15,33 @@ public class DetectVR : MonoBehaviour
     {
         if (startVR)
         {
-            var xrSettings = XRGeneralSettings.Instance;
-            if (xrSettings == null)
+            var xrSettings = XRGeneralSettings.Instance; //Get Instance of XRGeneralSettings
+            if (xrSettings == null)//if null
             {
-                Debug.Log("XR Settings null");
-                return;
+                Debug.Log("XR Settings null"); //Display error
+                return; //Stop
             }
 
-            var xrManager = xrSettings.Manager;
+            var xrManager = xrSettings.Manager; //Assign XRManager
             if (xrManager == null)
             {
-                Debug.Log("Manager is null");
-                return;
+                Debug.Log("Manager is null"); //Display error
+                return; //Stop
             }
 
-            var xrLoader = xrManager.activeLoader;
-            if (xrLoader == null)
+            var xrLoader = xrManager.activeLoader; //XR Loader (Load VR)
+            if (xrLoader == null) //If null
             {
                 Debug.Log("XRLoader is null");
-                ActivateDesktopCharacter();
+                ActivateDesktopCharacter(); //Activate Desktop character (NO VR)
             }
 
             Debug.Log("XRLoader not null");
-            desktopCharacter.SetActive(false);
-            xrOrigin.SetActive(true);
+            desktopCharacter.SetActive(false);//Disable NO VR
+            xrOrigin.SetActive(true); //Enable VR
            
             //Activate Headset
-            XRGeneralSettings.Instance.Manager.InitializeLoaderSync();
+            XRGeneralSettings.Instance.Manager.InitializeLoaderSync(); 
             XRGeneralSettings.Instance.Manager.StartSubsystems();
         }
         else
@@ -49,7 +49,7 @@ public class DetectVR : MonoBehaviour
             ActivateDesktopCharacter();
         }
     }
-
+    
     private void ActivateDesktopCharacter()
     {
         xrOrigin.SetActive(false);
